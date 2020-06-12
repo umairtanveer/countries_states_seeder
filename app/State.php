3cd77ff44cfe0e8f575b6381c2b9be2e2c5d8bdc
Matthias Ludwig <ml@mypromo-service.com>
2020-06-12 13:36:09 +0200

    combine countries with states

--- app/State.php
+++ app/State.php
@@ -7,11 +7,12 @@ use PragmaRX\Countries\Package\Countries;
 
 class State extends Model
 {
-    public static function states($countryCode){
+    public static function states($countryCode)
+    {
         $countries = new Countries();
-        try{
-            return  $countries->where('cca3', $countryCode)->first()->hydrateStates()->states->pluck('name','postal')->toArray();
-        }catch(\Exception $ex){
+        try {
+            return $countries->where('cca3', $countryCode)->first()->hydrateStates()->states->pluck('name', 'postal')->toArray();
+        } catch (\Exception $ex) {
             return [];
         }
     }
--- resources/views/countries.blade.php
+++ resources/views/countries.blade.php
@@ -5,21 +5,37 @@
     <title>Countries</title>
 </head>
 <body>
-@foreach($countries as $country)
+    @foreach($countries as $country)
+        <h4>// Country</h4>
 
-    DB::table('countries')->updateOrInsert(['iso2' => '{{ $country->code }}'], [ <br>
-    'name'           => '{{ str_replace("'","",$country->name) }}',<br>
-    'iso2'           => '{{ $country->code }}',<br>
-    'code'           => '{{ $country->iso3 }}',<br>
-    'continent_name' => '{{ $country->continent->name }}',<br>
-    @if($country->continent_code == 'EU')
-        'eu'             => true,<br>
-    @else
-        'eu'             => false,<br>
-    @endif
-    'created_at'     => Carbon::now(),<br>
-    ]);<br>
-    <br>
-@endforeach
+        $country = Country::updateOrCreate(['iso2' => '{{ $country->code }}'], [ <br>
+            'name'           => '{{ str_replace("'","",$country->name) }}',<br>
+            'iso2'           => '{{ $country->code }}',<br>
+            'code'           => '{{ $country->iso3 }}',<br>
+            'continent_name' => '{{ $country->continent->name }}',<br>
+            @if ($country->continent_code == 'EU')
+                'eu'             => true,<br>
+            @else
+                'eu'             => false,<br>
+            @endif
+        ]);
+        <br>
+        <br>
+
+        <h4>// States</h4>
+
+        @foreach ($country->states() as $key => $state)
+            $country->states()->updateOrCreate(['iso2' => '{{  $key }}', 'code' => '{{ $country->code }}-{{ $key }}'], [ <br>
+                'name'       => '{{ str_replace("'","",$state) }}',<br>
+                'iso2'       => '{{ $key }}',<br>
+                'code'       => '{{ $country->code }}-{{ $key }}',<br>
+            ]); <br>
+
+            <br>
+
+        @endforeach
+
+        <br>
+    @endforeach
 </body>
 </html>
